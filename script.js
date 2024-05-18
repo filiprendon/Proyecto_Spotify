648fdcbc003bbb945c7eaa1efbf1e9cf483d5111
filiprendon <126860088+filiprendon@users.noreply.github.com>
2024-05-18 23:48:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const token = '497bf12bd91d46e6bd57233a08cb6b61';
+async function fetchWebApi(endpoint, method, body) {
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method,
+      body:JSON.stringify(body)
+    });
+    return await res.json();
+  }
+  
+  async function getTopTracks(){
+    // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+    return (await fetchWebApi(
+      'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+    )).items;
+  }
+  
+  const topTracks = await getTopTracks();
+  console.log(
+    topTracks?.map(
+      ({name, artists}) =>
+        `${name} by ${artists.map(artist => artist.name).join(', ')}`
+    )
+  );
